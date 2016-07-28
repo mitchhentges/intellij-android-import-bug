c09abd6d207d2d8fe04ddf6e0780207d0313e629
Mitchell Hentges <mhentges@arrow.ca>
2016-07-28 12:28:41 -0700

    Add comments to code for video

--- android/src/main/java/ca/stti/test/TestActivity.java
+++ android/src/main/java/ca/stti/test/TestActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+// "Android" still builds fine when core isn't "Android Gradle"
 public class TestActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- core/src/main/java/ca/stti/test/Library.java
+++ core/src/main/java/ca/stti/test/Library.java
@@ -1,5 +1,9 @@
 package ca.stti.test;
 
+// Core being an "Android Gradle" module slows down core-only builds significantly
+//   (especially when there's more code)
+
+// Should only do "Make", not "gradle build using tasks"
 public class Library {
     public static String string() {
         return "from :core";
